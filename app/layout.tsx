4b2748445c1248b19e53bef54bda6f1ce01b57b8
prajyot-porje <prajyotyt18@gmail.com>
2026-04-26 16:38:21 +0530

    setting up tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,6 +49,15 @@ export default function RootLayout({
       <head>
         <script
           dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KFH89H3Z');`,
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
             __html: `
                (function() {
                  var t = localStorage.getItem('devstudio-theme');
@@ -60,6 +69,14 @@ export default function RootLayout({
         />
       </head>
       <body className={`${poppins.variable} ${inter.variable} antialiased`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KFH89H3Z"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <ThemeProvider>
           <SmoothScroll>
             <LoadingProvider>
